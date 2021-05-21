c4ad53acfdc0f66bfd442dd4a8b85e8964b10c83
Sam Machin <sam@sammachin.com>
2021-05-21 10:33:59 +0100

    websockets

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on ${port}`)
-})
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+ 
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+ 
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+ 
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+ 
+app.listen(8080);
